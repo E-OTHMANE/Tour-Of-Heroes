ec40fdfda328e45c6e61458b5aae0642768d977f
ESSOUFI Othmane <othmaneessoufi@gmail.com>
2022-03-17 11:31:41 +0100

    Detail Feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +15,14 @@ export class HeroesComponent implements OnInit {
     name : 'Windstorm'
   };
 
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void { this.selectedHero = hero; }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES:Hero[] = [
+  { id: 11, name: 'Dr.Strange' },
+  { id: 12, name: 'Iron-man' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Hawkeyes' },
+  { id: 17, name: 'Black Widow' },
+  { id: 18, name: 'Scarlet witch' },
+  { id: 19, name: 'Spider-man' },
+  { id: 20, name: 'Ant-man' }
+]
