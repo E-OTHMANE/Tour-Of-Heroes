1d6a213683a6e1dcf70ba3b584bba7eefff8135f
ESSOUFI Othmane <othmaneessoufi@gmail.com>
2022-03-18 09:39:37 +0100

    Update
    Getting what has been destroyed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero-service/hero.service';
 
 import { Hero } from '../models/hero';
 
@@ -9,14 +13,26 @@ import { Hero } from '../models/hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input()
-  hero? : Hero;
+  hero? : Hero | undefined;
 
-  constructor() { }
+  constructor (
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+    ) { }
 
   ngOnInit(): void {
+      this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 
+  goBack(): void {
+    this.location.back();
+  }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,12 @@ import { MessageService } from '../message-service/message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+/*
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
+*/
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -23,13 +23,13 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
-
+/*
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent : Selected hero id=${hero.id}`);
   }
-
+*/
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
