f485846a18c68239d3a6f4dfce5336f1afb8a88a
ESSOUFI Othmane <othmaneessoufi@gmail.com>
2022-03-20 21:41:16 +0100

    creation and update of services
    creation in-memory-data service and update the hero service

---
+++ src/app/In-memory-data/in-memory-data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from '../models/hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService  implements InMemoryDbService{
+
+  createDb() {
+
+    const heroes = [
+      { id: 11, name: 'Dr.Strange' },
+      { id: 12, name: 'Iron-man' },
+      { id: 13, name: 'Captain America' },
+      { id: 14, name: 'Thor' },
+      { id: 15, name: 'Hulk' },
+      { id: 16, name: 'Hawkeyes' },
+      { id: 17, name: 'Black Widow' },
+      { id: 18, name: 'Scarlet witch' },
+      { id: 19, name: 'Spider-man' },
+      { id: 20, name: 'Ant-man' }
+    ];
+    return heroes
+
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+
+
+  constructor() { }
+}
